81a6b54a2b7141f634ed2f798b6a80ece8d39388
Dan Nixon <dan@dan-nixon.com>
2025-03-13 15:24:20 +0000

    Add ability to steal peripherals to BSP
    Required for panic handlers.

---
+++ bsp/examples/steal/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::{Duration, Timer};
+use pico_plc_bsp::peripherals::PicoPlc;
+use portable_atomic as _;
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let p = unsafe { PicoPlc::steal() };
+
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        embassy_time::block_for(Duration::from_hz(5));
+        led.toggle();
+    }
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = PicoPlc::default();
+
+    info!("Hello, world!");
+
+    let mut led = Output::new(p.PIN_25, Level::Low);
+    led.set_high();
+
+    Timer::after_secs(5).await;
+
+    panic!("oh dear. how sad. nevermind.");
+}
